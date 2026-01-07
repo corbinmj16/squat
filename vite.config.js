b8f87e46223cbfefe98d8260ea1bf3e1e3839923
Corbin Jensen <corbin.jensen@me.com>
2026-01-07 11:59:38 -0500

    fixes paths

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // base: '/squat/', // IMPORTANT
+  base: '/squat/', // IMPORTANT for GitHub Pages
+  build: {
+    assetsDir: '' // Put assets in root of dist instead of /assets subfolder
+  }
 })
